5a21fc90dd010beefb0f1859c01e04aef5781286
Pushergene <pushergene@protonmail.com>
2020-07-06 23:24:29 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
-
 import "fmt"
 
 func main() {
-  fmt.Println("Hello World")
-}
+    var x int
+    x = 100
+        for l := 0; l < 20; l++ {
+        if(l + 5 == x) {
+            fmt.Print(l, "+ 5 == 14: ")
+            break;
+        }
+         if(l != 9) {
+            fmt.Println("%d", "ist nicht 9.", l)
+        }
+        
+     if(x > (10 / 5 + 20)) {
+        fmt.Println("x > (x / 1 + 20: %d ", l)
+    } 
+    }
+    }
